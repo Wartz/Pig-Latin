6ec6b3f0df9bc5a0b1fa06c7b5a63e3bd046a330
Wartz <wartzay@gmail.com>
2014-11-07 08:25:57 -0500

    Added in several TODOs for future developing.

--- src/com/joe/PigLatin/PigLatinRun.java
+++ src/com/joe/PigLatin/PigLatinRun.java
@@ -2,7 +2,7 @@ package com.joe.PigLatin;
 
 public class PigLatinRun {
     public static void main (String[] args) {
-        String out = PigLatinTranslate.translateToPL("America");
+        String out = PigLatinTranslate.translateToPL("Smith!");
         System.out.println(out);
     }
 }
--- src/com/joe/PigLatin/PigLatinTranslate.java
+++ src/com/joe/PigLatin/PigLatinTranslate.java
@@ -11,7 +11,7 @@ class PigLatinTranslate
         
         for(int i=0; i < words.length; i++) {
             String parts = "";
-            
+
             if (words[i].toLowerCase().equals(words[i])) {
                 parts += words[i].substring(1,2);
             }
@@ -19,12 +19,16 @@ class PigLatinTranslate
                 parts += words[i].substring(1,2).toUpperCase();
             }
 
+            /*TODO: Strip out punctuation and move it to the end of the phrase after parsing*/
             /*TODO: Use regex to check for words that start with vowels to add "way" instead of "ay" at the end.*/
 
+            //Translates a word by moving the first character of the word to the end and then adding "ay".
             parts += words[i].substring(2) + words[i].substring(0,1).toLowerCase() + "ay" + (i < words.length-1 ? " " : "");
             result += parts;
         }
 
+        /*TODO: Look into figuring out how to capitalize proper nouns, etc*/
+        //Formats a word or phrase to be capitalized.
         result = result.substring(0,1).toUpperCase() + result.substring(1).toLowerCase();
 
         return result;
